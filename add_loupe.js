796e3e673da5fc7810676d3c29b44319b3494167
KALAN <p.belan@iepi.ru>
2024-08-05 13:25:54 +0300

    Initial commit

---
+++ add_loupe.js
@@ -0,0 +1,37 @@
+function show_loupe(isOn){
+	const { Loupe, enableLoupe } = window.loupe
+	const useRectangle = new URLSearchParams(window.location.search).get("shape") === "rectangle"
+	const magnification = 1.5
+	const style = {
+		borderColor: "white",
+		boxShadow: "4px 5px 5px 4px rgba(0,0,0,0.5)",
+	}
+
+	const opt = useRectangle ? {
+		magnification,
+		style,
+	} : {
+		magnification,
+		shape: "circle",
+		width: 400,
+		height: 400,
+		container: this.document.body,
+		style,
+	}
+	
+	const l = new Loupe(opt)
+	const target = document.getElementById("map")
+
+	function toggle_loupe() {
+	  console.log(!isOn);
+	  if (!isOn) {
+		disableLoupe = enableLoupe(target, target.src, l);
+		console.log('Loupe Enabled');
+	  } else {
+		disableLoupe();
+		console.log('Loupe Disabled');
+	  }
+	}
+  
+	toggle_loupe();
+}
